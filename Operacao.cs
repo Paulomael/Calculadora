df5742a03331f461b93ce58f18b0600a6afc894b
PAULO ISMAEL RIBEIRO DE FREITAS <116131@pmenos.com.br>
2021-11-04 09:01:04 -0300

    Commit Incial

---
+++ Operacao.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+
+    public class Operacao
+    {
+        
+        public void  Somar(List<double> x)
+        {
+            double resultado = 0;
+            foreach (var item in x)
+            {
+                resultado += item;
+            }
+
+            Console.WriteLine(resultado);
+        }
+
+
+
+
+        public void Subtrair(List<double> x)
+        {
+            double resultado = 0;
+            foreach (var item in x)
+            {
+                resultado -= item;
+            }
+
+            Console.WriteLine(resultado);
+        }
+
+
+        public void Multiplicar(List<double> x)
+        {
+            double resultado = x[0];
+            
+
+            for (int i = 1; i < x.Count; i++)
+            {
+                resultado *= x[i];
+            }
+
+            Console.WriteLine(resultado);
+        }
+
+
+        public void Dividir(double x,double y)
+        {
+            if (y == 0)
+            {
+                Console.WriteLine("Impossivel Dividir Por Zero");
+            }
+            else
+            {
+                Console.WriteLine($"O Resultado é {x / y}");
+            }                                                                       
+        }
+    }
+}
+
+
+
+
+
---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculadora !");
+
+            Operacao Op = new Operacao();
+            List<double> numeros = new List<double>();
+
+            string opcao;
+            int n = 1;
+
+            do
+            {
+                Console.WriteLine($"Informe {n} Numero");
+                double numero1 = double.Parse(Console.ReadLine());
+                numeros.Add(numero1);
+
+                do
+                {
+                    Console.WriteLine("Deseja Informar outro numero ? sim ou nao");
+
+                    opcao = Console.ReadLine();
+                    if (opcao == "sim")
+                    {
+                        n += 1;
+                    }
+                    else if (opcao != "nao")
+                    {
+                        Console.WriteLine("Opção Invalida !");
+                    }
+                } while (opcao != "sim" && opcao != "nao");
+
+
+            } while (opcao != "nao");
+           
+            Console.WriteLine("Digite : 1 - Somar \nDigite : 2 - Subtrair \nDigite : 3 - Multiplicar \nDigite : 4 - Dividir \n");
+
+            string Escolha = Console.ReadLine();
+
+            switch (Escolha)
+            {
+                case "1":
+                    Op.Somar(numeros);
+                    break;
+                case "2":
+                    Op.Subtrair(numeros);
+                    break;
+                case "3":
+                    Op.Multiplicar(numeros);
+                    break;
+                case "4":
+                    if (numeros.Count < 2)
+                    {
+                        Console.WriteLine("Não é Possivel Efetuar a Operação");
+                    }
+                    else
+                    {
+                        if (numeros.Count > 2)
+                        {
+                            Console.WriteLine($"So foi Possivel Realizar a operação com os numeros {numeros[0]} e {numeros[1]}");
+                        }
+                        Op.Dividir(numeros[0], numeros[1]);
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Opção Incorreta");
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
